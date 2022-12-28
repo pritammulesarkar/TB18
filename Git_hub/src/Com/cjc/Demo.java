f500d655313f130d3dfce9dc62d11a688f1f179b
HP <HP@DESKTOP-BQSDL3S>
2022-12-28 15:48:24 +0530

    By XYZ

--- Git_hub/src/Com/cjc/Demo.java
+++ Git_hub/src/Com/cjc/Demo.java
@@ -1,7 +1,11 @@
 package Com.cjc;
 
 public class Demo {
-public static void main(String[] args) {
-	System.out.println("Hello Git");
-}
+	public void m1() {
+		System.out.println("Hello m1");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
 }
