74209026cb59c056e65b03192df8f982a2642299
pritammulesarkar <121536859+pritammulesarkar@users.noreply.github.com>
2022-12-28 15:51:33 +0530

    Update Demo.java

--- Git_hub/src/Com/cjc/Demo.java
+++ Git_hub/src/Com/cjc/Demo.java
@@ -4,6 +4,9 @@ public class Demo {
 	public void m1() {
 		System.out.println("Hello m1");
 	}
+	public void m2() {
+		System.out.println("Hello m2");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
